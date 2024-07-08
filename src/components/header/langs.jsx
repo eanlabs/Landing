69dde71b5b07cd15510b88a12ade463f871bf83d
Eanlabs <eanlabs@protonmail.com>
2024-07-08 13:23:32 +0300

    add galery, contact, form

---
+++ src/components/header/langs.jsx
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2024, Eanlabs
+ * All rights reserved.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the 3-Clause BSD
+ * (also "New BSD", or "Modified BSD") License (BSD-3-Clause)
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * https://opensource.org/licenses/BSD-3-Clause
+ *
+ * @author    Eanlabs
+ * @copyright 2024, Eanlabs
+ * @license   https://opensource.org/licenses/BSD-3-Clause (3-Clause BSD License)
+ */
+
+import React from 'react';
+
+function Langs(props) {
+    return (
+        <div></div>
+    );
+}
+
+export default Langs;
